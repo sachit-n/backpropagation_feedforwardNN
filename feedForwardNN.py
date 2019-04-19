316f2040949d43abc5e0c25e8582f7ddd48d753f
Sachit Nagpal <sachitnagpal@Sachits-MacBook-Pro.local>
2019-04-19 22:34:39 +0530

    computed deltas (derivative of cost fn wrt z) for each layer

--- feedForwardNN.py
+++ feedForwardNN.py
@@ -21,13 +21,17 @@ Steps:
 '''
 
 #%%
+#Reference : http://deeplearning.stanford.edu/wiki/index.php/Backpropagation_Algorithm
+
+#%%
 
 import numpy as np
 from math import e
 from scipy.misc import derivative
-
+from collections import deque
 
 #%%
+#A couple of activation functions
 def sigmoid(x):
     return 1/(1+e**(-x))
 
@@ -52,24 +56,74 @@ class network:
             self.W[l] =  np.array([np.random.normal(loc=0, scale=0.1, size=self.layerN[l]) for i in range(self.layerN[l+1])]) #generate n random nos from N(0,0.1) and store in layerN[l+1]xlayerN[l] dimension matrix
     
     #Perform forward propagation
-    def forward_prop(self,X):                                                     #Compute Z vector for the first layer. Store it in key 1 of dictionary Z
+    def forward_prop(self,X):                                                               #Compute Z vector for the first layer. Store it in key 1 of dictionary Z
         self.A[0] = np.array(X)                                                             #Compute activation vector for the first layer. Store it in key 1 of dictionary A
-        for i in range(1, len(self.W)+1):
-            self.Z[i] = self.W[i-1]@self.A[i-1].T                                       #Compute Z vector for the layer i
-            self.A[i] = np.array([self.activation(i) for i in self.Z[i]])                 #Compute activation vector for the layer i. The last activation vector is the predicted Y.
+        for i in range(1, len(self.layerN)):
+            self.Z[i] = self.W[i-1]@self.A[i-1].T                                           #Compute Z vector for the layer i
+            self.A[i] = np.array([self.activation(i) for i in self.Z[i]])                   #Compute activation vector for the layer i. The last activation vector is the predicted Y.
     
     #compute weight gradients using backpropagation        
-    def compute_gradient(self, X, y):
-        np.dot(-(y-self.A[len(self.A)]), derivative(sigmoid, self.Z[len(self.Z)], dx=1e-6))
-        return "weight_gradients"
+    def compute_gradient(self,y):
+        deltas = deque([])
+        deltas.appendleft(np.multiply(-(y-self.A[len(self.A)-1]), derivative(self.activation, self.Z[len(self.Z)], dx=1e-6)))
+        for i in range(len(self.layerN)-2,0, -1):
+            deltas.appendleft(np.multiply(derivative(self.activation, self.Z[i]), self.W[i].T@deltas[0]))        
+        
     
     def fit(self, X, y, epoch=20):
         self.W = 'final_weights'
         
     def predict(self, X):
         return self.A[len(self.A)]
-        
-            
+    
+    
+    
+#%%
+from time import time
+
+t = time()
+a = {}
+for i in range(10000000):
+    a[i] = sigmoid(i)
+print(time()-t)
+    
+t = time()
+b = []
+for i in range(10000000):
+    b.append(sigmoid(i))
+print(time()-t)
+
+t = time()
+c = np.array([np.empty((1,10000000)), np.empty((1,10000000))])
+for i in range(10000000):
+    c[0][0,i] = sigmoid(i)
+print(time()-t)
+
+c = np.empty((1,10000000))
+t = time()
+for i in range(10000000):
+    c[0,i] = sigmoid(i)
+print(time()-t)
+
+
+t = time()
+for i in range(10000000):
+    m = a[i]
+print(time()-t)
+    
+t = time()
+for i in range(10000000):
+    m = b[i]
+print(time()-t)
+
+
+t = time()
+for i in range(10000000):
+    m = c[0,i]
+print(time()-t)
+
+
+                
     
             
     
